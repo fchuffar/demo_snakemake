1be82b2b712b2afc9945dbe9d5d7955691fa0119
Florent Chuffart <florent.chuffart@gmail.com>
2024-09-26 16:21:14 +0200

    Fixing metaanalyis

--- 02nd_worflow.py
+++ 02nd_worflow.py
@@ -23,31 +23,26 @@
 
 localrules: target
 
-
-
 import os 
 import os.path
-
 prefix = os.getcwd()
 
-# simulator parameters
-
+# parameter sweeps
 ncells = ["100000"]
 ncells = ["500"]
-ncells = ["100"]
+# ncells = ["100"]
 mis    = ["1"]
 sijs   = ["100"] 
 ds     = ["1"]
 fs     = ["50"]
 lambs  = ["1"]
-
 gamma1s = ["0", "1"]
 gamma2s = ["0", "500"]
 seeds   = ["0", "1", "2"]
 # seeds = [f"{seed}" for seed in range(8)]
-
 htmls = [f"{prefix}/mediation_analysis_lm_{ncell}_{i}_{sij}_{gamma1}_{gamma2}_{d}_{f}_{lamb}_rep{seed}.html" for ncell in ncells for i in mis for sij in sijs for gamma1 in gamma1s for gamma2 in gamma2s for d in ds for f in fs for lamb in lambs for seed in seeds]
 
+# Go snakemake, GO!!
 rule target:
   threads: 1
   message: "-- Execute the DAG. --"
@@ -82,7 +77,6 @@ RCODE="
 echo $RCODE | Rscript - 2>&1 > {log}
 """
 
-
 rule mediation_analysis_lm:
     input:
       rmd   = "{prefix}/mediation_analysis_lm.Rmd",
@@ -93,6 +87,8 @@ rule mediation_analysis_lm:
       rout  = "{prefix}/mediation_analysis_lm_{ncell}_{i}_{sij}_{gamma1}_{gamma2}_{d}_{f}_{lamb}_rep{seed}.Rout"    ,
       infos = "{prefix}/infos_{ncell}_{i}_{sij}_{gamma1}_{gamma2}_{d}_{f}_{lamb}_rep{seed}.rds",
       fit   = "{prefix}/fitmed_{ncell}_{i}_{sij}_{gamma1}_{gamma2}_{d}_{f}_{lamb}_rep{seed}.rds",
+    log:
+      rout  = "{prefix}/mediation_analysis_lm_{ncell}_{i}_{sij}_{gamma1}_{gamma2}_{d}_{f}_{lamb}_rep{seed}.Rout"    ,
     threads: 1
     shell:"""
 export OMP_NUM_THREADS=1
@@ -104,7 +100,7 @@ cd {wildcards.prefix}
 RCODE="
   ncell = '{wildcards.ncell}'; i = '{wildcards.i}'; sij = '{wildcards.sij}'; gamma1 = '{wildcards.gamma1}'; gamma2 = '{wildcards.gamma2}'; d = '{wildcards.d}'; f = '{wildcards.f}'; lamb = '{wildcards.lamb}'; seed = {wildcards.seed}; source('params.R');
   rmarkdown::render('mediation_analysis_lm.Rmd', output_file=paste0('mediation_analysis_lm_', suffix, '.html'), intermediates_dir=paste0('tmp_', suffix))"
-echo $RCODE | Rscript - 2>&1 > {output.rout}
+echo $RCODE | Rscript - 2>&1 > {log}
 """
 
 rule clean:
